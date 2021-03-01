474f3e4503f65c5dfe400a77859ac2818457b34d
Michal Tichý <email@tichymichal.net>
2021-03-01 19:07:50 +0100

    datapager samples

--- Controls/builtin/DataPager/sample2/ViewModel.cs
+++ Controls/builtin/DataPager/sample2/ViewModel.cs
@@ -32,10 +32,25 @@ namespace DotvvmWeb.Views.Docs.Controls.builtin.DataPager.sample2
 
         public override Task Init()
         {
-            // NOTE: You can also create the DataSet with factory.
-            // Just call static Create with delegate and pagesize.
-            Customers = GridViewDataSet.Create(gridViewDataSetLoadDelegate: GetData, pageSize: 4);
+            if (!Context.IsPostBack)
+            {
+
+                Customers = new GridViewDataSet<Customer>()
+                {
+                    PagingOptions = new PagingOptions()
+                    {
+                        PageSize = 4
+                    }
+                };
+            }
             return base.Init();
+        }
+
+        public override Task PreRender()
+        {
+            Customers.LoadFromQueryable(FakeDb());
+
+            return base.PreRender();
         }
 
     }
