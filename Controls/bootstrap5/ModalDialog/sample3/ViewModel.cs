98fc8cbcc16ae42d8bd00e6dd75380a25df7ba31
František Siladi <frantisek.siladi@riganti.cz>
2022-10-03 13:12:18 +0200

    ModalDialog bootstrap5 docs

---
+++ Controls/bootstrap5/ModalDialog/sample3/ViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DotvvmWeb.Views.Docs.Controls.bootstrap5.ModalDialog.sample1
+{
+    public class ViewModel
+    {
+         public bool Displayed { get; set; } = false;
+    }
+}
