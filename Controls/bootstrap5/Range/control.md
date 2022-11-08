f99ca2489203e55c29fb9e96aa690bbca0f4f2ec
František Siladi <frantisek.siladi@riganti.cz>
2022-11-08 13:24:58 +0100

    Range docs

---
+++ Controls/bootstrap5/Range/control.md
@@ -0,0 +1,3 @@
+Renders the Bootstrap range form control.
+
+<https://getbootstrap.com/docs/5.2/forms/range/#>
---
+++ Controls/bootstrap5/Range/sample2/ViewModel.cs
@@ -0,0 +1,6 @@
+﻿public class ViewModel : DotvvmViewModelBase
+{
+    public double Value { get; set; } = 1000;
+    public int ValueChanges { get; set; } = 0;
+    public bool Enabled { get; set; } = true;
+}
