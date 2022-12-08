4dd837747e5eb087ca59ea7bff7153907a13cd47
Maxim Duzij <maxim.duzij@hotmail.com>
2022-12-08 15:31:12 +0100

    Icon docs added

---
+++ Controls/bootstrap5/Icon/sample2/ViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using DotVVM.Framework.Controls;
+using DotVVM.Framework.ViewModel;
+
+namespace DotvvmWeb.Views.Docs.Controls.bootstrap.DataPager.sample1
+{
+    public class ViewModel : DotvvmViewModelBase
+    {
+         public Icons PostbackIcon { get; set; } = Icons.Clipboard;
+
+        public string ButtonText { get; set; } = "Copy to clipboard";
+        public void ChangeIcon()
+        {
+            PostbackIcon = PostbackIcon == Icons.Clipboard ? Icons.Check : Icons.Clipboard;
+            ButtonText = "Copied!";
+        }
+    }
+}
