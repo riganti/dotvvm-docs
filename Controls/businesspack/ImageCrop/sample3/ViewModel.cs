12c5d646c05de733e7d1b2a2eb756a6516d37f0c
Tomáš Herceg <tomas.herceg@riganti.cz>
2022-12-09 14:36:11 +0100

    Fixed image URL in ImageCrop

--- Controls/businesspack/ImageCrop/sample3/ViewModel.cs
+++ Controls/businesspack/ImageCrop/sample3/ViewModel.cs
@@ -6,7 +6,7 @@ namespace DotvvmWeb.Views.Docs.Controls.businesspack.ImageCrop.sample3
 {
     public class ViewModel : DotvvmViewModelBase
     {
-        public string ImagePath { get; set; } = "../../images/imagecrop.webp";
+        public string ImagePath { get; set; } = "../images/imagecrop.webp";
         public ImageOperations ImageOperations { get; set; } = new ImageOperations();
     }
 }
--- Controls/businesspack/ImageCrop/sample5/ViewModel.cs
+++ Controls/businesspack/ImageCrop/sample5/ViewModel.cs
@@ -6,7 +6,7 @@ namespace DotvvmWeb.Views.Docs.Controls.businesspack.ImageCrop.sample5
     public class ViewModel : DotvvmViewModelBase
     {
         public int ChangeCount { get; set; }
-        public string ImagePath { get; set; } = "../../images/imagecrop.webp";
+        public string ImagePath { get; set; } = "../images/imagecrop.webp";
         public ImageOperations ImageOperations { get; set; } = new ImageOperations();
 
         public void Changed()
